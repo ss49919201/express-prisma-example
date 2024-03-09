1b252fe853af504c2a3458cafd262af5f589c91c
ss49919201 <sakashin10291029@gmail.com>
2024-03-09 22:58:25 +0900

    chore: Prisma Seed 導入

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+async function main() {
+  await prisma.user.create({
+    data: {
+      name: "Alice",
+      tasks: {
+        create: {
+          status: "TODO",
+        },
+      },
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
